33ec8cd042c38f3b6c18c3338306c97e859ed0a5
Alvaro <aechavarris4@gmail.com>
2020-10-28 15:32:26 +0100

    Version de 27/10

---
+++ DB_Test (27-10)/src/es/unizar/sisinf/grp1/model/SolicitudVO.java
@@ -0,0 +1,80 @@
+package es.unizar.sisinf.grp1.model;
+
+import java.util.Date;
+import java.sql.Time;
+
+/**
+ * tabla users
+ * @author sisinf
+ *
+ */
+public class SolicitudVO {
+	private int idSolicitud;
+	private int estado;
+	private int ss;
+	private String profesional;
+	private Date dia;
+	private java.sql.Time hora;
+	/**
+	 * Constructor
+	 * @param userName
+	 * @param password
+	 */
+	public SolicitudVO(int idSolicitud, int estado, int ss, String profesional, Date dia, java.sql.Time hora) {
+		this.idSolicitud = idSolicitud;
+		this.estado = estado;
+		this.ss = ss;
+		this.profesional = profesional;
+		this.dia = dia;
+		this.hora = hora;
+	}
+
+	public int getID() {
+		return idSolicitud;
+	}
+
+	public void setID(int id) {
+		this.idSolicitud = id;
+	}
+	
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	
+	public void setSS(int ss) {
+		this.ss = ss;
+	}
+
+	public int getSS() {
+		return ss;
+	}
+
+	public String getProfesional() {
+		return profesional;
+	}
+
+	public void setProfesional(String profesional) {
+		this.profesional = profesional;
+	}
+
+	public Date getDia() {
+		return dia;
+	}
+
+	public void setDia(Date dia) {
+		this.dia = dia;
+	}
+
+	public java.sql.Time getHora() {
+		return hora;
+	}
+
+	public void setHora(java.sql.Time hora) {
+		this.hora = hora;
+	}
+	
+}
